9e9287aec63569d360d42394f6aa0e8548fcdef9
Simon Guest <guestsi@i11019.agresearch.co.nz>
2013-05-13 11:12:15 +1200

    Conditionally compile OpenMP related code only on platforms which support it.

--- src/omp_set_num_threads_ptr.c
+++ src/omp_set_num_threads_ptr.c
@@ -1,6 +1,13 @@
+#include <Rconfig.h>
+#ifdef SUPPORT_OPENMP
 #include <omp.h>
+#endif
 
-/* a C routine callable from R to set number of OpenMP threads */
+/* a C routine callable from R to set number of OpenMP threads 
+ * - does nothing on platforms which don't support OpenMP
+ */
 void omp_set_num_threads_ptr(int *np) {
+#ifdef SUPPORT_OPENMP
   omp_set_num_threads(*np);
+#endif
 }
